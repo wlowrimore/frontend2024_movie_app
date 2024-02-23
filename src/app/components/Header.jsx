31b7fc05b5447b82432c64c5f46640d5e1ba457b
William Lowrimore <wlowrimore@gmail.com>
2024-02-22 23:01:11 -0600

    fixes some sizing issues in header and adds placeholder user image for users with no gravatar

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -15,17 +15,25 @@ const Header = async () => {
         <div className='flex text-zinc-200 items-center gap-10'>
           {session ? (
             <>
-              <div className='text-indigo-200 flex gap-6'>
-                <p className='py-1 px-2'>Welcome,&nbsp; {session.user.name}</p>
+              <div className='text-indigo-200 flex items-center gap-2'>
+                <p className='py-1 px-2 font-light tracking-wide'>{session.user.name}</p>
                 {session?.user?.image ? (
                   <Image
                     src={session.user.image}
                     alt={session.user.name}
                     width={40}
                     height={40}
-                    className='rounded-full w-6 mr-10'
+                    className='rounded-full w-8 h-8'
                   />
-                ) : null}
+                ) : (
+                  <Image
+                    src='/images/user.svg'
+                    alt={session.user.name}
+                    width={40}
+                    height={40}
+                    className='rounded-full w-6 h-6'
+                  />
+                )}
               </div>
               <div className=''>
                 <Link href='/api/auth/signout' className='py-1 px-2'><h3 className='text-indigo-100 hover:text-indigo-300 transition duration-300'>SignOut</h3></Link>
