905c1ca5f229efe40df7bee070c887e4115dd341
William Lowrimore <wlowrimore@gmail.com>
2024-03-10 23:49:43 -0500

    sets up localStorage for trial signup and adds success modal and conditional styling

--- src/app/components/forms/TrialForm.jsx
+++ src/app/components/forms/TrialForm.jsx
@@ -4,8 +4,56 @@ import { useState, useEffect } from 'react'
 import CountriesDropdown from '../ui/CountriesDropdown';
 import TermsCheckbox from '../ui/TermsCheckbox';
 const TrialForm = ({ handleCloseForm }) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [country, setCountry] = useState('')
+  const [isChecked, setIsChecked] = useState(false)
+  const [successMsg, setSuccessMsg] = useState('')
 
+  const [trialClaimantData, setTrialClaimantData] = useState([]);
 
+  const handleCheckboxChange = (e) => {
+    setIsChecked(e.target.checked)
+  }
+
+  const handleSelectCountry = (e) => {
+    setCountry(e.target.value)
+  }
+
+  const removeFormAndMsg = () => {
+    setSuccessMsg(null)
+    handleCloseForm()
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    const newEntry = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      country: country,
+      isChecked: isChecked
+    }
+
+    setTrialClaimantData([...trialClaimantData, newEntry])
+
+    if (trialClaimantData) {
+
+      setSuccessMsg(`Thank you ${firstName.toUpperCase()}! Your free trial is now active!`)
+    }
+  }
+
+  useEffect(() => {
+    const existingData = JSON.parse(localStorage.getItem('trialClaimantData')) || [];
+
+    if (Array.isArray(trialClaimantData)) {
+      const combinedData = [...existingData, ...trialClaimantData];
+      localStorage.setItem('trialClaimantData', JSON.stringify(combinedData));
+    } else {
+      console.error('trialClaimantData is not an array.');
+    }
+  }, [trialClaimantData])
 
   useEffect(() => {
     document.body.style.overflow = 'hidden'
@@ -17,6 +65,12 @@ const TrialForm = ({ handleCloseForm }) => {
 
   return (
     <main className='fixed z-20 inset-0 backdrop-blur-sm flex items-center justify-center bg-neutral-900/60'>
+      {successMsg ? (
+        <div className='fixed inset-44 bg-gradient-to-r from-blue-700 to-red-800 flex flex-col gap-10 items-center justify-center rounded-2xl border-2 border-zinc-200'>
+          <h3 className='text-5xl text-white'>{successMsg}</h3>
+          <button onClick={removeFormAndMsg} className='px-5 py-7 rounded-full text-zinc-50 text-lg tracking-wider font-bold bg-slate-900/30 border-2 border-zinc-50 hover:bg-green-500/50 transition duration-300'>Start</button>
+        </div>
+      ) : null}
       <div className='flex bg-slate-100/50 p-28 w-4/5 border-2 border-zinc-300 rounded-lg shadow-xl shadow-neutral-800'>
         <div className='flex flex-col'>
           <div className='w-4/5'>
@@ -31,18 +85,46 @@ const TrialForm = ({ handleCloseForm }) => {
             </ul>
           </div>
         </div>
-        <form className='flex flex-col space-y-4'>
-          <div className='flex items-center gap-4'>
-            <input className='p-2 border-2 border-zinc-300 rounded-lg' type="text" placeholder='First Name' />
-            <input className='p-2 border-2 border-zinc-300 rounded-lg' type="text" placeholder='Last Name' />
+        <form onSubmit={handleFormSubmit} className='flex flex-col space-y-4'>
+          <div className='flex items-center gap-4'>([
+            {
+              firstName,
+              lastName,
+              email,
+              country,
+              isChecked
+            }
+            ])
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              className='p-2 border-2 border-zinc-300 rounded-lg'
+              placeholder='First Name' />
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className='p-2 border-2 border-zinc-300 rounded-lg'
+              placeholder='Last Name' />
           </div>
-          <input className='p-2 border-2 border-zinc-300 rounded-lg' type="email" placeholder='Email' />
-          <CountriesDropdown />
-          <TermsCheckbox />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='p-2 border-2 border-zinc-300 rounded-lg'
+            placeholder='Email' />
+          <CountriesDropdown handleSelectCountry={handleSelectCountry} />
+          <TermsCheckbox handleCheckboxChange={handleCheckboxChange} />
           <div className='flex gap-8 mx-auto'>
-            <button className='py-3 px-[4.8rem] bg-red-500 text-xl text-white border rounded-lg hover:bg-red-700  transition duration-300'>Claim Now</button>
+            <button
+              disabled={!isChecked}
+              className={`py-3 px-[4.8rem] text-xl text-white border rounded-lg transition duration-300 ${isChecked ? 'bg-red-600/80 hover:bg-red-700' : 'bg-red-600/30 border-none cursor-not-allowed'}`}>Claim Now</button>
 
-            <button onClick={handleCloseForm} className='py-3 px-[6.4rem] bg-slate-700 text-xl text-white border rounded-lg hover:bg-slate-800 transition duration-300'>Cancel</button>
+            <button onClick={handleCloseForm}
+              className='py-3 px-[6.4rem] bg-slate-700 text-xl text-white border rounded-lg hover:bg-slate-800 transition duration-300'>
+              Cancel
+            </button>
           </div>
           <em className='text-xs text-slate-700'>&dagger; we will never sell or share your information</em>
         </form>
--- src/app/components/ui/TermsCheckbox.jsx
+++ src/app/components/ui/TermsCheckbox.jsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
 
-const TermsCheckbox = () => {
+const TermsCheckbox = ({ handleCheckboxChange }) => {
   return (
     <div className='flex text-sm items-center gap-2'>
-      <input type="checkbox" name="terms" id="terms" className='w-4 h-4 cursor-pointer' />
+      <input onClick={handleCheckboxChange} type="checkbox" name="terms" id="terms" className='w-4 h-4 cursor-pointer' />
       <label htmlFor="terms">I have read the <Link href='#' className='text-zinc-800 underline hover:text-zinc-600 transition duration-200'>terms and conditions</Link> and hereby agree.</label>
     </div>
   )
